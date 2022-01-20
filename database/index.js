d4f822f7c3d32885e343335528f969815d125e0e
stan_bk <2568143457@qq.com>
2022-01-20 11:36:39 +0800

    fix: 修改db模块返回值

--- database/index.js
+++ database/index.js
@@ -4,6 +4,7 @@ const { testDatabaseConnect } = require('./connection')
 async function db(sequelize, DataTypes) {
   const model = defineSequelizeModel(sequelize, DataTypes)
   await testDatabaseConnect(sequelize) 
+  return model
 }
 
 module.exports = db
