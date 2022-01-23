3f3e3e0186e9381331c23325bac16583f743e4c8
stan_bk <2568143457@qq.com>
2022-01-23 20:24:35 +0800

    style: 改动项目文件结构

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const Koa = require('koa')
 const app = new Koa()
-const { config } = require('./src/database/database.config')
+
 const { Sequelize, DataTypes } = require('sequelize')
-const db = require('./src/database/index')
+const { config } = require('./database/database.config')
+const db = require('./database/index')
+const router = require('./routes/index')
+
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: config.dialect,
@@ -14,8 +17,10 @@ let model // 接收sequelize模型
 
 db(sequelize, DataTypes).then(data => {
   model = data
-  const operate = require('./src/database/operate')(model) // 传入数据表模型，获得CRUD操作对象
+  const operate = require('./database/operate')(model) // 传入数据表模型，获得CRUD操作对象
+  app.use(router.routes(), router.allowedMethods())
   app.listen(3001)
+
 }).catch(error => {
   console.log('数据库同步出错:', error)
 })
