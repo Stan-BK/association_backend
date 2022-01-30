f9a738d58d57ed09f213d1c36d7453e06dfd3865
stan_bk <2568143457@qq.com>
2022-01-30 21:20:06 +0800

    fix: 添加关联查询选项

--- controller/database/operate.js
+++ controller/database/operate.js
@@ -3,9 +3,10 @@ const operate = function(model) {
     const person = await model[table].create({...options})
     console.log('增加了新条目:', person.toJSON())
   }
-  async function Select(table, options, where, include) {
+  async function Select(table, options, where, include, association) {
     const person = await model[table].findAll({
       attributes: include ? { include: include } : options,
+      include: association ? association : null,
       where
     })
     console.log('查询结果:', JSON.stringify(person, null, 2))
