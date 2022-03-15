52eecd064fdc698e96bf76881f199ead02b7864e
stan_bk <2568143457@qq.com>
2022-03-15 12:47:58 +0800

    update: 更新用户注册路由逻辑

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,11 @@ const ResModel = require('../model/response')
 const { generateToken, validate } = require('../src/user/user')
 const generateError = require('../src/error')
 
+// 生成初始随机昵称的备用字符
+const strUpc = Array(26).fill(65).map((num, index) => String.fromCodePoint(num + index))
+const strLwc = Array(26).fill(97).map((num, index) => String.fromCodePoint(num + index))
+const strNum = Array(10).fill(0).map((num, index) => num + index)
+
 router.post('/user/login', async (ctx) => {
   const operate = ctx.db.operate
   const user = ctx.request.body
@@ -48,24 +53,29 @@ router.put('/user/register', async (ctx, next) => {
   const operate = ctx.db.operate
   const user = ctx.request.body
   try {
+    if (user.username === '' || user.password === '') {
+      throw new Error('用户名和密码不能为空')
+    }
+    if (user.password !== user.confirmpsw) {
+      throw new Error('两次密码输入不一致')
+    }
     await operate['Insert']('user', {
       avatar: user.avatar,
       username: user.username,
       password: user.password,
-      nickname: user.nickname,
+      nickname: generateRandomNickname(),
       user_role: 1
     })
     const token = await generateToken(user.username, false) // 根据用户名生成token
     ctx.body = new ResModel().succeed(token)
   } catch(e) {
     let error = generateError(e)
-    const obj = {
-      'username': '用户名',
-      'nickname': '昵称'
-    }
-    const field = obj[error.split(' ')[0]]
-    error = field + '已存在'
     ctx.body = new ResModel().err(undefined, error)
+  }
+
+  function generateRandomNickname() {
+    const str = Array.from({ length: 13 }).map(() => strUpc.concat(strLwc, strNum)[Math.floor(Math.random() * (26 + 26 + 10))]).join('')
+    return 'ank_' + str
   }
 })
 
